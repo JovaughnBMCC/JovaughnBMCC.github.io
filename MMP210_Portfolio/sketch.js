307a3ea657d28ef031dc660c4544f7b96ea2469e
JovaughnBMCC <98756700+JovaughnBMCC@users.noreply.github.com>
2022-03-25 00:31:58 -0400

    Update sketch.js

--- MMP210_Portfolio/sketch.js
+++ MMP210_Portfolio/sketch.js
@@ -15,8 +15,8 @@ function setup() {
 noStroke();
   fill(0,0,0,30)
    cnv = createCanvas(600, 700);
+    gif_createImg.parent("container");
     cnv.parent("container");
-   gif_createImg.parent("container");
  
 
 }
